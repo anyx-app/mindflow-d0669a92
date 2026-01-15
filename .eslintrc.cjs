e364f4b6a4f29055ed8d2fc557f97d7d59370ba2
Michael Kisilenko <mrkisilenko@gmail.com>
2026-01-15 12:20:37 +0200

    feat: Relax strict lint rules by default (v1.14.15 prep)

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -23,5 +23,8 @@ module.exports = {
   settings: {
     react: { version: 'detect' },
   },
-  rules: {},
+  rules: {
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-unused-vars': 'warn',
+  },
 }; 
